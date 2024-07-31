dbe667886fec402a31a0fcb2188d3e3ebe268f15
jardenna <hbjornum@gmail.com>
2024-07-31 10:02:16 +0200

    deleted unused files

--- src/pages/commonPages/Links.tsx
+++
@@ -1,5 +0,0 @@
-import { FC } from 'react';
-
-interface LinksProps {}
-const Links: FC<LinksProps> = () => <div>Links</div>;
-export default Links;
--- src/pages/commonPages/Thanks.tsx
+++
@@ -1,5 +0,0 @@
-import { FC } from 'react';
-
-interface ThanksProps {}
-const Thanks: FC<ThanksProps> = () => <div>Thanks</div>;
-export default Thanks;
