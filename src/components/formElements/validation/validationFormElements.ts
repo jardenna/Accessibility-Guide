5ba79d10c6d167b7e0c6f4701a6689b146659bdd
jardenna <hbjornum@gmail.com>
2024-09-27 13:20:31 +0200

    validation

---
+++ src/components/formElements/validation/validationFormElements.ts
@@ -0,0 +1,27 @@
+import { FormValues, ValidationErrors } from '../../../hooks/useFormValidation';
+import { CommonText } from '../../../types/lang';
+import { emailRegex } from '../../../utils';
+
+function validationFormElements(values: FormValues) {
+  const errors: ValidationErrors = {};
+  const { phone, email, fullName } = values;
+
+  if (!phone) {
+    errors.phone = `${CommonText.PleaseEnter} Your phone no.`;
+  }
+
+  if (!fullName) {
+    errors.fullName = `${CommonText.PleaseEnter} Your name`;
+  }
+
+  // Email Errors
+  if (!email) {
+    errors.email = `${CommonText.PleaseEnter} Your email`;
+  } else if (!emailRegex.test(email as string)) {
+    errors.email = `${CommonText.PleaseEnter} a valid emai`;
+  }
+
+  return errors;
+}
+
+export default validationFormElements;
